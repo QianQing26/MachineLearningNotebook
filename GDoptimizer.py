16da11880436b3fc0b250863011b2d353d736945
QianQing26 <eevee_hbc@outlook.com>
2023-10-14 20:40:12 +0800

    10/14

--- GDoptimizer.py
+++ GDoptimizer.py
@@ -6,7 +6,7 @@ class SGD:
         self.lr = lr
     
     def update(self,w,b,dw,db):
-        w -= self.lr*dw
+        w -=self.lr*dw
         b -= self.lr*db
     
 
@@ -122,3 +122,168 @@ class Adam:
         self.m['b'] += (1-self.beta1)*(db-self.m['b'])
         self.v['b'] += (1-self.beta2)*(db**2 - self.v['b'])
         b -= lr_t * self.m['b'] / (np.sqrt(self.v['b'])+1e-7)
+
+
+class AdaMax:
+    """AdaMax"""
+    def __init__(self, lr=0.002, beta1=0.9, beta2=0.999):
+        self.lr = lr
+        self.beta1 = beta1
+        self.beta2 = beta2
+        self.iter = 0
+        self.m = None
+        self.v = None
+    
+    def update(self, w, b, dw, db):
+        if self.m is None:
+            self.m, self.v = {}, {}
+            self.m['w'] = np.zeros_like(w)
+            self.m['b'] = np.zeros_like(b)
+            self.v['w'] = np.zeros_like(w)
+            self.v['b'] = np.zeros_like(b)
+        self.iter += 1
+        lr_t = self.lr / (1.0 - self.beta1**self.iter)
+        self.m['w'] = self.beta1*self.m['w'] + (1-self.beta1)*dw
+        self.v['w'] = np.maximum(self.beta2*self.v['w'], np.abs(dw))
+        w -= lr_t * self.m['w'] / (self.v['w'] + 1e-7)
+        self.m['b'] = self.beta1*self.m['b'] + (1-self.beta1)*db
+        self.v['b'] = np.maximum(self.beta2*self.v['b'], np.abs(db))
+        b -= lr_t * self.m['b'] / (self.v['b'] + 1e-7)
+
+
+class Adadelta:
+    def __init__(self, rho=0.95, epsilon=1e-6):
+        self.rho = rho
+        self.epsilon = epsilon
+        self.E_dw = None
+        self.E_db = None
+        self.E_dw_update = None
+        self.E_db_update = None
+    
+    def update(self, w, b, dw, db):
+        if self.E_dw is None:
+            self.E_dw = np.zeros_like(w)
+            self.E_db = np.zeros_like(b)
+            self.E_dw_update = np.zeros_like(w)
+            self.E_db_update = np.zeros_like(b)
+        
+        self.E_dw = self.rho*self.E_dw + (1-self.rho)*dw**2
+        self.E_db = self.rho*self.E_db + (1-self.rho)*db**2
+        dw_update = -np.sqrt(self.E_dw_update + self.epsilon)/np.sqrt(self.E_dw + self.epsilon)*dw
+        db_update = -np.sqrt(self.E_db_update + self.epsilon)/np.sqrt(self.E_db + self.epsilon)*db
+        w += dw_update
+        b += db_update
+        self.E_dw_update = self.rho*self.E_dw_update + (1-self.rho)*dw_update**2
+        self.E_db_update = self.rho*self.E_db_update + (1-self.rho)*db_update**2
+
+
+class Adafactor:
+    def __init__(self, learning_rate=None, beta1=0.9, beta2=0.999, epsilon=1e-8, regularization=None, decay_rate=-0.8):
+        self.learning_rate = learning_rate
+        self.beta1 = beta1
+        self.beta2 = beta2
+        self.epsilon = epsilon
+        self.regularization = regularization
+        self.decay_rate = decay_rate
+        self.m = None
+        self.v = None
+        self.t = None
+    
+    def update(self, w, b, dw, db):
+        if self.m is None:
+            self.m = np.zeros_like(w)
+            self.v = np.zeros_like(w)
+            self.t = 0
+        
+        self.t += 1
+        self.m = self.beta1*self.m + (1-self.beta1)*dw
+        self.v = self.beta2*self.v + (1-self.beta2)*(dw**2)
+        lr = self.learning_rate
+        if self.learning_rate is None:
+            lr = (self.learning_rate/np.sqrt(self.t))
+        
+        if self.regularization == "l1":
+            w -= lr * (self.m/np.sqrt(self.v + self.epsilon) + self.decay_rate * np.sign(w))
+        elif self.regularization == "l2":
+            w -= lr * (self.m/np.sqrt(self.v + self.epsilon) + self.decay_rate * w)
+        else:
+            w -= lr * (self.m/np.sqrt(self.v + self.epsilon))
+        self.m = self.beta1*self.m + (1-self.beta1)*db
+        self.v = self.beta2*self.v + (1-self.beta2)*(db**2)
+        lr = self.learning_rate
+        if self.learning_rate is None:
+            lr = (self.learning_rate/np.sqrt(self.t))
+        
+        if self.regularization == "l1":
+            b -= lr * (self.m/np.sqrt(self.v + self.epsilon) + self.decay_rate * np.sign(b))
+        elif self.regularization == "l2":
+            b -= lr * (self.m/np.sqrt(self.v + self.epsilon) + self.decay_rate * b)
+        else:
+            b -= lr * (self.m/np.sqrt(self.v + self.epsilon))
+
+
+class AMSGrad:
+    def __init__(self, learning_rate=0.001, beta1=0.9, beta2=0.999, epsilon=1e-8):
+        self.learning_rate = learning_rate
+        self.beta1 = beta1
+        self.beta2 = beta2
+        self.epsilon = epsilon
+        self.m = None
+        self.v = None
+        self.v_hat = None
+    
+    def update(self, w, b, dw, db):
+        if self.m is None:
+            self.m = np.zeros_like(w)
+            self.v = np.zeros_like(w)
+            self.v_hat = np.zeros_like(w)
+        
+        self.m = self.beta1*self.m + (1-self.beta1)*dw
+        self.v = self.beta2*self.v + (1-self.beta2)*(dw**2)
+        self.v_hat = np.maximum(self.v_hat, self.v)
+        w -= self.learning_rate * (self.m/(np.sqrt(self.v_hat) + self.epsilon))
+        self.m = self.beta1*self.m + (1-self.beta1)*db
+        self.v = self.beta2*self.v + (1-self.beta2)*(db**2)
+        self.v_hat = np.maximum(self.v_hat, self.v)
+        b -= self.learning_rate * (self.m/(np.sqrt(self.v_hat) + self.epsilon))
+
+
+class RAdam:
+    def __init__(self, learning_rate=0.001, beta1=0.9, beta2=0.999, epsilon=1e-8, decay_rate=-0.8):
+        self.learning_rate = learning_rate
+        self.beta1 = beta1
+        self.beta2 = beta2
+        self.epsilon = epsilon
+        self.decay_rate = decay_rate
+        self.iter = 0
+        self.rho_inf = 2/(1-self.beta2) - 1
+        self.rho_t = None
+        self.m = None
+        self.v = None
+    
+    def update(self, w, b, dw, db):
+        if self.m is None:
+            self.m = np.zeros_like(w)
+            self.v = np.zeros_like(w)
+        
+        self.iter += 1
+        rho_t = self.rho_inf - 2*self.iter*self.beta2**self.iter / (1-self.beta2**self.iter)
+        self.m = self.beta1*self.m + (1-self.beta1)*dw
+        self.v = self.beta2*self.v + (1-self.beta2)*(dw**2)
+        if rho_t > 4:
+            r = np.sqrt(((rho_t-4)*(rho_t-2)*self.rho_inf)/((self.rho_inf-4)*(self.rho_inf-2)*rho_t))
+            w -= self.learning_rate * r * (self.m/(np.sqrt(self.v) + self.epsilon) + self.decay_rate * w)
+        else:
+            w -= self.learning_rate * (self.m/(np.sqrt(self.v) + self.epsilon) + self.decay_rate * w)
+        self.m = self.beta1*self.m + (1-self.beta1)*db
+        self.v = self.beta2*self.v + (1-self.beta2)*(db**2)
+        if rho_t > 4:
+            r = np.sqrt(((rho_t-4)*(rho_t-2)*self.rho_inf)/((self.rho_inf-4)*(self.rho_inf-2)*rho_t))
+            b -= self.learning_rate * r * (self.m/(np.sqrt(self.v) + self.epsilon) + self.decay_rate * b)
+        else:
+            b -= self.learning_rate * (self.m/(np.sqrt(self.v) + self.epsilon) + self.decay_rate * b)
+
+'''
+L-BFGS
+Proximal Gradient Descent
+'''
--- model.py
+++ model.py
@@ -4,6 +4,7 @@ import itertools
 import GDoptimizer
 
 class LinearRegression:
+    '''线性回归'''
     def __init__(self,learning_rate=0.01,num_iterations=50,batch_size=16):
         self.learning_rate = learning_rate
         self.num_iterations = num_iterations
@@ -62,6 +63,7 @@ class LinearRegression:
 
 
 class PolynomialFeatures:
+    '''生成多项式特征'''
     def __init__(self, degree=2):
         self.degree = degree
     
@@ -79,3 +81,147 @@ class PolynomialFeatures:
                 X_poly = np.hstack((X_poly, new_feature))
         
         return X_poly[:, 1:]
+
+
+class LogisticRegression:
+    '''逻辑分类'''
+    def __init__(self):
+        self.w = None
+        self.label =None
+    
+    def sigmoid(self, x):
+        return 1/(1+np.exp(-x))
+    
+    def set_label(self,y):
+        self.label = np.unique(y)
+
+    def init_weights(self, num_features):
+        limits = 1/np.sqrt(num_features)
+        self.w = np.random.uniform(-limits,limits,(num_features+1,1))
+        self.w[0]=0
+
+    def fit(self, X, y, num_iteration=5000, batch_size = 16,optimizer=GDoptimizer.Adam()):
+        num_samples, num_features = X.shape
+        self.init_weights(num_features)
+
+        self.set_label(y)
+        y_hat = y
+        for i in range(len(self.label)):
+            y_hat[y_hat==self.label[i]]=i
+        X_hat = np.insert(X, 0, 1, axis=1)
+        y_hat = np.reshape(y_hat, (num_samples, 1))
+
+        for _ in range(num_iteration):
+            mask = np.random.choice(num_samples,batch_size)
+            x_batch = X_hat[mask]
+            y_batch = y_hat[mask]
+            grad = self.grad(x_batch, y_batch)
+            optimizer.update(self.w, 0.0,grad,0.0)
+
+    def grad(self, X, y):
+        h_x = X.dot(self.w)
+        y_pred = self.sigmoid(h_x)
+        return X.T.dot(y_pred-y)/X.shape[0]
+    
+    def predict(self, X):
+        X_hat = np.insert(X,0,1,axis=1)
+        y = self.sigmoid(-(X_hat.dot(self.w)))
+        y = np.round(y)
+        y.astype('int')
+        y = np.reshape(y,(1,-1))[0]
+        y[y==1.]=self.label[1]
+        y[y==0.]=self.label[0]
+        return y
+
+
+class MultiLogisticRegresstion:
+    '''使用softmax的多分类'''
+    def __init__(self):
+        self.w = None
+        self.label = None
+    
+    def softmax(self,x):
+        if x.ndim == 2:
+            x = x.T
+            x = x - np.max(x, axis=0)
+            y = np.exp(x) / np.sum(np.exp(x), axis=0)
+            return y.T
+
+        x = x - np.max(x)  # 溢出对策
+        return np.exp(x) / np.sum(np.exp(x))
+
+    def set_label(self,y):
+        self.label = np.unique(y)
+
+    def compute_prob(self,X):
+        scores = X.dot(self.w)
+        return self.softmax(scores)
+    
+    def compute_grad(self,X,y,regularization_param):
+        num_samples = X.shape[0]
+        probs = self.compute_prob(X)
+        grad = -(1/num_samples)*X.T.dot(np.eye(self.w.shape[1])[y]-probs)
+        regularization_term = regularization_param/num_samples*self.w
+        grad+=regularization_term
+        return grad 
+    
+    def initialize_weights(self,num_samples, num_features):
+        limits = 1/np.sqrt(num_samples**2+num_features**2)
+        self.w = np.random.uniform(-limits,limits,(num_features+1,num_samples))
+    
+    def fit(self,X,y,num_iterations=5000,regularization_param=0.01,batch_size=16,optimizer=GDoptimizer.SGD()):
+        num_samples, num_features = X.shape
+        X_hat = np.insert(X,0,1,axis=1)
+        y_hat = y
+        self.set_label(y_hat)
+        for i in range(len(self.label)):
+            y_hat[y_hat==self.label[i]]=i
+        self.initialize_weights(num_samples, num_features)
+
+        for i in range(num_iterations):
+            mask = np.random.choice(num_samples,batch_size)
+            x_batch = X_hat[mask]
+            y_batch = y_hat[mask]
+            grad = self.compute_grad(x_batch,y_batch,regularization_param)
+            optimizer.update(self.w,0,grad,0)
+    
+    def predict(self,X):
+        X_hat = np.insert(X,0,1,axis=1)
+        y_pred = self.compute_prob(X_hat)
+        return self.label[np.argmax(y_pred, axis=1)]
+
+
+
+class KNN:
+    def __init__(self, K=5):
+        self.k = K
+        self.X = None
+        self.y = None
+
+    def fit(self, X, y):
+        self.X = X
+        self.y = y
+
+    def euclidean_distance(self, x1, x2):
+        dis = x1-x2
+        dis = dis*dis
+        dis = np.sum(dis)
+        return np.sqrt(dis)
+
+    def predict(self,x):
+        y_pred = np.zeros(x.shape[0])
+
+        for i in range(x.shape[0]):
+            distances = np.zeros((self.X.shape[0], 2))
+
+            for j in range(self.X.shape[0]):
+                dis = self.euclidean_distance(x[i], self.X[j])
+                label = self.y[j]
+                distances[j] = [dis, label]
+            
+            k_nearest_neighbors = distances[distances[:,1].argsort()][:self.k]
+            counts = np.bincount(k_nearest_neighbors[:,1].astype('int'))
+            testlabel = counts.argmax()
+            y_pred[i] = testlabel
+        
+        return y_pred
